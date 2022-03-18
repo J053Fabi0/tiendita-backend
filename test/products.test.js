50491f754778192fdbeba76de5fb0c15f8b60e63
Amiko <senorbinario@tuta.io>
2022-03-17 17:33:47 -0700

    Test POST /product

---
+++ test/products.test.js
@@ -0,0 +1,45 @@
+const app = require("../index");
+const request = require("supertest");
+const { whipeData } = require("./testUtils");
+const { tagsDB, categoriesDB, productsDB } = require("../db/collections/collections");
+
+beforeEach(whipeData);
+
+describe("POST /product", () => {
+  it("should specify json as the content type in the http header", async () => {
+    const response = await request(app).post("/product").send({ name: "b", price: 1, stock: 1 });
+    expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
+  });
+
+  it("should respond with the $loki of the new product", async () => {
+    const response = await request(app).post("/product").send({ name: "b", price: 1, stock: 1 });
+    expect(response.body.message).toBe(1);
+  });
+
+  it("should exists in DB", async () => {
+    expect(productsDB.findOne({ $loki: 1 })).toBeNull();
+    await request(app).post("/product").send({ name: "b", price: 1, stock: 1 });
+    expect(productsDB.findOne({ $loki: 1 })).toBeTruthy();
+  });
+
+  it("only possible tags are allowed", async () => {
+    tagsDB.insertOne({ name: "a" });
+    tagsDB.insertOne({ name: "b" });
+    const response = await request(app)
+      .post("/product")
+      .send({ name: "b", price: 1, stock: 1, tags: [3] });
+    expect(response.body.error.description).toBe("Validation error: 'tags[0]' must be one of [1, 2]");
+  });
+});
+
+describe("GET /product", () => {
+  describe("when the product exists", () => {
+    // should specify json as the content type in the http header
+    // it should give the product when it is not enabled
+    // it should give the product when it is enabled
+  });
+
+  describe("when the product doesn't exists", () => {
+    // it should give an error
+  });
+});
