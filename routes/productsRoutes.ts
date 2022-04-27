827f3f65a376bd6c73bcac98d5d3f49ac32240bd
Amiko <senorbinario@tuta.io>
2022-04-26 18:29:02 -0600

    Auth for products

--- routes/productsRoutes.ts
+++ routes/productsRoutes.ts
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import * as s from "../schemas/productsSchema";
 import * as c from "../controllers/productsController";
+import { authJWTAllRoles, authJWTOnlyAdmins } from "../middlewares/authJWT";
 
 const productsRoutes = Router();
 
-productsRoutes.get("/product", s.getProduct, c.getProduct);
-productsRoutes.get("/products", s.getProducts, c.getProducts);
+// productsRoutes.get("/product", s.getProduct, c.getProduct);
+productsRoutes.get("/products", authJWTAllRoles, s.getProducts, c.getProducts);
 
-productsRoutes.post("/product", s.postProduct, c.postProduct);
+productsRoutes.post("/product", authJWTOnlyAdmins, s.postProduct, c.postProduct);
 
-productsRoutes.delete("/product", s.deleteProduct, c.deleteProduct);
+// productsRoutes.delete("/product", s.deleteProduct, c.deleteProduct);
 
-productsRoutes.patch("/product", s.patchProduct, c.patchProduct);
+// productsRoutes.patch("/product", s.patchProduct, c.patchProduct);
 
 export default productsRoutes;
